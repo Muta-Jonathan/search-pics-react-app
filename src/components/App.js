551125f471799e44263d9e4a04f6fa0bc624ffb9
Muta-Jonathan <mutajonathan4@gmail.com>
2023-07-05 01:16:26 +0300

    Added render image list

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import SearchBar from "./SearchBar";
 import unsplash from "../api/unsplash";
+import ImageList from "./ImageList";
 
 class App extends React.Component {
   state = { images: [] };
@@ -11,13 +12,12 @@ class App extends React.Component {
       },
     });
     this.setState({ images: response.data.results });
-    console.log(response.data.results);
   };
   render() {
     return (
       <div className="ui container">
         <SearchBar onSubmit={this.onSearchSubmit} />
-        Found: {this.state.images.length}
+        <ImageList images={this.state.images} />
       </div>
     );
   }
---
+++ src/components/ImageList.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ImageList = (props) => {
+  console.log(props.images);
+
+  const images = props.images.map((image) => {
+    return <img key={image.id} src={image.urls.regular} />;
+  });
+
+  return <div>{images}</div>;
+};
+
+export default ImageList;
