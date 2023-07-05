35f20c78eb93fb47cc08c8791ecc40d7fd99112a
Muta-Jonathan <mutajonathan4@gmail.com>
2023-07-05 03:45:22 +0300

    used Ref in styling

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,13 +1,15 @@
 import React from "react";
+import ImageCard from "./ImageCard";
+import "../css/ImageList.css";
 
 const ImageList = (props) => {
   console.log(props.images);
 
   const images = props.images.map((image) => {
-    return <img key={image.id} src={image.urls.regular} />;
+    return <ImageCard key={image.id} image={image} />;
   });
 
-  return <div>{images}</div>;
+  return <div className="image-list">{images}</div>;
 };
 
 export default ImageList;
